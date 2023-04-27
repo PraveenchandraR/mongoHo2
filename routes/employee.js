54d0da9c171da8d76356abbf1662408b2c67a14a
Praveen <praveenchandraravuri@gmail.com>
2023-04-27 18:39:27 +0530

    mongoHo2

---
+++ routes/employee.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const controller = require('../controller/employee');
+const router = express.Router();
+
+router.post('/addnewemployees', controller.saveEmployeeData);
+router.get('/showallemployees', controller.getAllEmployeesData);
+router.get('/highersalary', controller.getHighSalaryEmployees);
+router.get('/experienced', controller.getMoreExperienceEmployees);
+router.get('/graduatedafter2015', controller.getGraduatedAfter2015);
+router.put('/updatesalary', controller.updateSalary);
+router.delete('/deleteEmployees', controller.deleteEmployeeFromY);
+
+
+module.exports = router;
